3ad5666143496b13c0892911d607818f5393b1ea
ian1105102 <ian105102@gmail.com>
2024-12-26 01:35:40 +0800

    add deep link

--- app/src/main/java/com/example/friendloop/QrActivity.java
+++ app/src/main/java/com/example/friendloop/QrActivity.java
@@ -1,6 +1,11 @@
 package com.example.friendloop;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,21 +13,70 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
 public class QrActivity extends AppCompatActivity {
 
+
+    Button mButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_qr);
+
+
+
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-
-
-
-
             return insets;
         });
+
+        mButton = findViewById(R.id.button3);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IntentIntegrator integrator = new IntentIntegrator(QrActivity.this);
+                    integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)
+                            .setOrientationLocked(true)
+                            .setPrompt("請將 QR Code 對準掃描框")
+                            .setCameraId(0)
+                            .setBeepEnabled(true)
+                            .initiateScan(); // 啟動掃描
+
+            }
+        });
+
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            if (result.getContents() != null) {
+                // 顯示掃描結果
+                String qrCodeContent = result.getContents();
+
+                if (qrCodeContent.startsWith("friendloop://add")) {
+
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(qrCodeContent));
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(this, "掃描成功: " + qrCodeContent, Toast.LENGTH_LONG).show();
+                }
+
+            } else {
+                // 用戶取消掃描
+                Toast.makeText(this, "掃描取消", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
